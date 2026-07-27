1a486ef7006420f8d6ddc99577a3d89522fe8690
prud-Owner <linuxeop@gmail.com>
2026-07-27 11:33:59 +0530

    some changes

--- lua/options.lua
+++ lua/options.lua
@@ -69,6 +69,11 @@ vim.o.scrolloff = 15
 -- See `:help 'confirm'`
 vim.o.confirm = true
 vim.opt.laststatus = 3
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = false
 -- vim: ts=2 sts=2 sw=2 et
 --
 --
